b89e1f17e19342d7ba5b72537accae7acee4f356
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2025-05-24 14:58:24 -0500

    Cambio

--- src/Pages/Calendar/UserCalendar.jsx
+++ src/Pages/Calendar/UserCalendar.jsx
@@ -67,7 +67,7 @@ export default function UserCalendar() {
         id_cita: appointment.id,
         fecha: fecha,
         hora: hora,
-        estado: appointment.estado.toLowerCase(), // Convertir a minúsculas para mantener consistencia
+        estado: appointment.estado.toLowerCase(), // Convertir a minúsculas para mantener consistencia.
         nombre_negocio: appointment.business.nombre,
         cliente_nombre: appointment.client.nombre,
         cliente_email: appointment.client.email,
